168975c5b5f133445adfc516dfb9a42b5199562b
kimberly lin <kimberly.lin10@gmail.com>
2021-09-30 14:26:31 -0700

    added put and delete routes

--- routes/companies.js
+++ routes/companies.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const router = express.Router();
 
-const db = require("../db.js")
+const db = require("../db.js");
+const { NotFoundError } = require("../expressError.js");
 
 
 /** GET list of companies / => {companies: [{code, name}, ...]} */
@@ -34,7 +35,7 @@ router.get("/:code", async function (req, res, next) {
   return res.json({ company });
 });
 
-/** Create new company, return company */
+/** Create new company, return {company: {code, name, description}} */
 
 router.post("/", async function (req, res, next) {
   const { code, name, description } = req.body;
@@ -47,6 +48,38 @@ router.post("/", async function (req, res, next) {
   );
   const company = result.rows[0];
   return res.status(201).json({ company });
+});
+
+/** Edit existing company details, return {company: {code, name, description}} 
+ * or 404 if not found */
+router.put("/:code", async function (req, res, next) {
+  const { name, description } = req.body;
+
+  const result = await db.query(
+    `UPDATE companies
+           SET name = $1,
+               description = $2
+           WHERE code = $3
+           RETURNING code, name, description`,
+    [name, description, req.params.code]
+  );
+  const company = result.rows[0];
+
+  if (!company) throw new NotFoundError(`${req.params.code} is not found`);
+
+  return res.status(201).json({ company });
+});
+
+/** Delete company, return {status: "deleted"} or 404 if company not found*/
+router.delete("/:code", async function (req, res, next) {
+  const compCode = req.params.code;
+
+  const result = await db.query("SELECT * FROM companies WHERE code=$1", [compCode]);
+  console.log(result);
+  if (!result.rows[0]) throw new NotFoundError(`${compCode} is not found`);
+
+  await db.query("DELETE FROM companies WHERE code=$1", [compCode]);
+  return res.json({ status: "deleted" })
 });
 
 module.exports = router;
