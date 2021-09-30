d33d639e26be94641bed0965765cd32d66562f97
kimberly lin <kimberly.lin10@gmail.com>
2021-09-30 12:14:02 -0700

    added routes for specific company details and creating a new company

--- routes/companies.js
+++ routes/companies.js
@@ -6,18 +6,47 @@ const db = require("../db.js")
 
 /** GET list of companies / => {companies: [{code, name}, ...]} */
 
-router.get("/", async function (req, res, next){
-    //select all from DB
-    // return list of all per below
+router.get("/", async function (req, res, next) {
+  //select all from DB
+  // return list of all per below
 
-    const result = await db.query(
-        `SELECT * FROM companies`
-    );
+  const result = await db.query(
+    `SELECT * FROM companies`
+  );
 
-    const companies = result.rows;
+  const companies = result.rows;
 
-    return res.json({companies});
-  });
+  return res.json({ companies });
+});
 
+/** GET a company with company code / => {company: {code, name, description}} */
+router.get("/:code", async function (req, res, next) {
+  //select using company code
+  const compCode = req.params.code;
+  const result = await db.query(
+    `SELECT code, name, description 
+    FROM companies 
+    WHERE code = $1`, [compCode]
+  );
 
-  module.exports = router;
+  const company = result.rows[0];
+
+  return res.json({ company });
+});
+
+/** Create new company, return company */
+
+router.post("/", async function (req, res, next) {
+  const { code, name, description } = req.body;
+
+  const result = await db.query(
+    `INSERT INTO companies (code, name, description)
+           VALUES ($1, $2, $3)
+           RETURNING code, name, description`,
+    [code, name, description],
+  );
+  const company = result.rows[0];
+  return res.status(201).json({ company });
+});
+
+module.exports = router;
